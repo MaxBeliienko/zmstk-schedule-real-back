8a14c151d7ac85cde8ce98bcb22ae9f961437fd7
MaxBeliienko <mbelienko@gmail.com>
2026-07-17 10:57:26 +0300

    edit port

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
   res.send("API Автошколи працює!");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
